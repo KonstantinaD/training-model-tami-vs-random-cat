c7acab20ffabb797e2a8e4d9ba37696a12742004
Konstantina Dureva <k_dureva@yahoo.com>
2022-09-01 19:57:28 +0100

    align

--- sketch_30.js
+++ sketch_30.js
@@ -14,27 +14,27 @@ function setup() {
     createCanvas(400, 400);
     // image(tamiImages[0], 0, 0, width, height);
 
-const IMAGE_WIDTH = 64;
-const IMAGE_HEIGHT = 64;
-const IMAGE_CHANNELS = 4;
+    const IMAGE_WIDTH = 64;
+    const IMAGE_HEIGHT = 64;
+    const IMAGE_CHANNELS = 4;
 
-let options = {
-    inputs: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-    // outputs: ['label'],
-    task: 'imageClassification',
-    debug: true
-};
+    let options = {
+        inputs: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        // outputs: ['label'],
+        task: 'imageClassification',
+        debug: true
+    };
 
-imageClassifier = ml5.neuralNetwork(options);
+    imageClassifier = ml5.neuralNetwork(options);
 
-for (let i = 0; i < tamiImages.length; i++) {
-    imageClassifier.addData({ image: tamiImages[i] }, { label: 'Tami' });
-    imageClassifier.addData({ image: otherCatImages[i] }, { label: 'A Random Cat' });
-}
+    for (let i = 0; i < tamiImages.length; i++) {
+        imageClassifier.addData({ image: tamiImages[i] }, { label: 'Tami' });
+        imageClassifier.addData({ image: otherCatImages[i] }, { label: 'A Random Cat' });
+    }
 
-imageClassifier.normalizeData();
-console.log(imageClassifier);
-imageClassifier.train({ epochs: 50 }, finishedTraining);
+    imageClassifier.normalizeData();
+
+    imageClassifier.train({ epochs: 50 }, finishedTraining);
 }
 
 function finishedTraining() {
